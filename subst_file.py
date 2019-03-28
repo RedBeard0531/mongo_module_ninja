602d7e55a434c0b8802d00859f93e3690967a437
Mathew Robinson <chasinglogic@gmail.com>
2019-03-28 14:18:30 -0400

    SERVER-32295 Support python 3

--- subst_file.py
+++ subst_file.py
@@ -7,13 +7,13 @@ import re
 import json
 
 if len(sys.argv) != 4:
-    print sys.argv[0] + ': in out json_subs'
+    print(sys.argv[0] + ': in out json_subs')
     sys.exit(1)
 
-with file(sys.argv[1]) as f:
+with open(sys.argv[1]) as f:
     contents = f.read()
 out_file_name = sys.argv[2]
-with file(sys.argv[3]) as f:
+with open(sys.argv[3]) as f:
     vars = json.load(f)
     subs = vars['subs']
     do_chmod = vars['do_chmod']
@@ -27,11 +27,11 @@ for k,v in subs:
 
 # Don't write to the file if it isn't changing
 if os.path.exists(out_file_name) and not do_chmod:
-    with file(out_file_name) as f:
+    with open(out_file_name) as f:
         if f.read() == contents:
             sys.exit(0)
 
-with file(out_file_name, 'w') as f:
+with open(out_file_name, 'w') as f:
     f.write(contents)
 
 if do_chmod:
